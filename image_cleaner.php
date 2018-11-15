1de394602712006c4e8312e3f76b90b8aa2b7c55
Vladimir Noskov <kovspace@gmail.com>
2018-11-16 00:12:43 +0300

    Update image_cleaner.php

--- image_cleaner.php
+++ image_cleaner.php
@@ -198,18 +198,20 @@ function checkDatabase($pathName) {
         if ($row) $isFound = TRUE;
     }
 
-    if (!$isFound) {
-        // Check in table 'property_value_files'
-        $oCore_QueryBuilder_Select = Core_QueryBuilder::select('file', 'file_small')
-            ->from('property_value_files')
-            ->open()
-            ->where('file', '=', $fileName)
-            ->setOr()
-            ->where('file_small', '=', $fileName)
-            ->close()
-            ->limit(1);
-        $row = $oCore_QueryBuilder_Select->execute()->asAssoc()->current();
-        if ($row) $isFound = TRUE;
+    if (strstr($pathName, 'property_')) {
+        if (!$isFound) {
+            // Check in table 'property_value_files'
+            $oCore_QueryBuilder_Select = Core_QueryBuilder::select('file', 'file_small')
+                ->from('property_value_files')
+                ->open()
+                ->where('file', '=', $fileName)
+                ->setOr()
+                ->where('file_small', '=', $fileName)
+                ->close()
+                ->limit(1);
+            $row = $oCore_QueryBuilder_Select->execute()->asAssoc()->current();
+            if ($row) $isFound = TRUE;
+        }
     }
 
     if (!$isFound) return TRUE;
