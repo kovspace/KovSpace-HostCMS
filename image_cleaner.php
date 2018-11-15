3b3d27542f4a5215f190a4a194e3742dcb7186f1
Vladimir Noskov <kovspace@gmail.com>
2018-11-15 19:31:21 +0300

    Update image_cleaner.php

--- image_cleaner.php
+++ image_cleaner.php
@@ -123,9 +123,6 @@ foreach ($aFiles as $file) {
 }
 ?>
 
-<!DOCTYPE html>
-<html lang="en">
-<head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>KovSpace Image Cleaner</title>
