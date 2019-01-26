0d55c97818c5ca5213a8755493a22f1454e47a1a
Vladimir Noskov <kovspace@gmail.com>
2019-01-26 04:54:52 +0300

    dater

---
+++ dater.php
@@ -0,0 +1,23 @@
+<?php
+class KovSpace_Dater
+{
+    public static function plural($n, $form1, $form2, $form3)
+    {
+        $n = abs($n) % 100;
+        $n1 = $n % 10;
+
+        if ($n > 10 && $n < 20) {
+            return $form3;
+        }
+
+        if ($n1 > 1 && $n1 < 5) {
+            return $form2;
+        }
+
+        if ($n1 == 1) {
+        return $form1;
+        }
+
+        return $form3;
+    }
+}
