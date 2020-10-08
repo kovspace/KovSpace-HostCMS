7953bcbf0a13886d119a543c9d6a37fd88d331b0
Vladimir Noskov <kovspace@gmail.com>
2020-10-08 17:58:37 +0300

    Update markingdecoder.php

--- markingdecoder.php
+++ markingdecoder.php
@@ -13,19 +13,25 @@ class KovSpace_MarkingDecoder
 
     public function __construct($source)
     {
+
+        if (strlen($source) < 31) $this->error = 'Код маркировки короче 31 символа';
+
         $this->gtin = substr($source, 2, 14);
+
+        if (!is_numeric($this->gtin)) $this->error = 'GTIN не является числом';
+
         $this->serial = substr($source, 18, 13);
-        $this->gtin16 = $this->hexFormat(0 . dechex($this->gtin));
+
+        $gtin_diff = strlen($this->gtin) - strlen((int)$this->gtin);
+        $gtin_hex = dechex($this->gtin);
+
+        for ($i=0; $i < $gtin_diff; $i++) {
+            $gtin_hex = "0$gtin_hex";
+        }
+
+        $this->gtin16 = $this->hexFormat($gtin_hex);
         $this->serial16 = $this->hexFormat(bin2hex($this->serial));
         $this->productCode = "$this->type $this->gtin16 $this->serial16";
-
-        if (strlen($source) < 31) {
-            $this->error = 'Код маркировки короче 31 символа';
-        }
-
-        if (!is_numeric($this->gtin)) {
-            $this->error = 'GTIN не является числом';
-        }
     }
 
     public function hexFormat($hex) {
