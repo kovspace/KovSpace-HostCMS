bc407538d6d6ef65b8f7bd92150276bfe6945495
Vladimir Noskov <kovspace@gmail.com>
2022-12-06 11:05:16 +0300

    Update bootstrap.php

--- bootstrap.php
+++ bootstrap.php
@@ -38,6 +38,28 @@ class Core_Mail_Observer
 {
     static public function onBeforeSend($object)
     {
+        if (str_starts_with($object->getSubject(), 'HostCMS')) {
+            $now = new DateTime('now');
+            $nowF = $now->format('Y-m-d H:i:s');
+            $to = KovSpace_Function::getProtectedProperty($object, '_to');
+            $file = CMS_FOLDER . 'hostcmsfiles/logs/emails.json';
+            $emails = json_decode(file_get_contents($file), true) ?? [];
+            isset($emails[$nowF]) && die('Дубль времени');
+
+            // Оставляем только события в пределах 5 минут
+            foreach ($emails as $date => $email) {
+                $prev = new DateTime($date);
+                if ($now->getTimestamp() - $prev->getTimestamp() > 5 * 60) {
+                    unset($emails[$date]);
+                }
+            }
+
+            in_array($to, $emails) && die('Прошло слишком мало времени');
+
+            $emails = [$nowF => $to] + $emails;
+            file_put_contents($file, json_encode($emails, JSON_PRETTY_PRINT));
+        }
+
         if (isset($_SERVER['SERVER_NAME'])) {
             $object->from('noreply@'.$_SERVER['SERVER_NAME']);
         }
