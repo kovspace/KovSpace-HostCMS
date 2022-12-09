acde69bbae6605476ec7f6bbaafaa5a9f6eb2476
Vladimir Noskov <kovaldo@yandex.ru>
2022-12-09 22:29:43 +0300

    Update cron.php

--- cron.php
+++ cron.php
@@ -9,7 +9,16 @@ function mailJobs(): void
     foreach ($files as $file) {
         $content = file_get_contents($file);
         $oCore_Mail = unserialize($content);
-        if ($oCore_Mail instanceof Core_Mail) {
+        if ($oCore_Mail instanceof Core_Mail_Smtp) {
+            if (!$oCore_Mail->send()->getStatus()) {
+                (new Core_Mail_Sendmail)
+                    ->to($oCore_Mail->getTo())
+                    ->from($oCore_Mail->getFrom())
+                    ->subject($oCore_Mail->getSubject())
+                    ->message($oCore_Mail->getMessage())
+                    ->send();
+            }
+        } elseif ($oCore_Mail instanceof Core_Mail) {
             $oCore_Mail->send();
         }
         unlink($file);
