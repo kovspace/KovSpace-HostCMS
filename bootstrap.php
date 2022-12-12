0449bcc884bfc8a0c45f4c916688e0bd8b472845
Vladimir Noskov <kovspace@gmail.com>
2022-12-12 09:31:09 +0300

    Update bootstrap.php

--- bootstrap.php
+++ bootstrap.php
@@ -170,8 +170,13 @@ class Core_Mail_Observer
                 $dir = CMS_FOLDER . 'cron/jobs/mail';
                 $file = $dir . '/' . time() . rand(100, 999);
                 $content = serialize($object);
-                $chmod = defined('CHMOD') ? CHMOD : 755;
-                Core_File::mkdir($dir, $chmod, TRUE);
+                if (!defined('CHMOD')) {
+                    define('CHMOD', '0755');
+                }
+                if (!defined('CHMOD_FILE')) {
+                    define('CHMOD_FILE', '0644');
+                }
+                Core_File::mkdir($dir, CHMOD, TRUE);
                 Core_File::write($file, $content);
                 return $object;
             }
